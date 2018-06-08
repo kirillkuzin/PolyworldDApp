49cab90cd79ac572721c39dacc386a22e5efc1be
Kirill <offkirillkuzin@gmail.com>
2018-06-08 03:10:57 +0300

    Update ownership system. Added percent ownership.

--- contracts/Approves.sol
+++ contracts/Approves.sol
@@ -1,19 +1,30 @@
+/*
+author: Kirill Kuzin (https://github.com/kirillkuzin)
+
+TODO:
+1) Добавить комментарии
+*/
+
 pragma solidity ^0.4.21;
 
 import "./Ownable.sol";
 
 contract Approves is Ownable {
 
-    //User address => other user address => build ID => approve state
-    mapping(address => mapping(address => mapping(uint256 => bool))) public purchasesApprove;
+    //User address => other user address => build ID => approve percent
+    mapping(address => mapping(address => mapping(uint256 => uint256))) public purchasesApprove;
 
-    modifier isPurchaseApprove(address _buyer, address _seller, uint256 _buildId) {
-        require(purchasesApprove[_buyer][_seller][_buildId]);
+    modifier isPurchaseApprove(address _buyer, address _seller, uint256 _buildId, uint256 _percent) {
+        require(purchasesApprove[_buyer][_seller][_buildId] >= _percent);
         _;
     }
 
-    function setPurchaseApprove(address _buyer, address _seller, uint256 _buildId) public onlyOwner {
-        purchasesApprove[_buyer][_seller][_buildId] = true;
+    function setPurchaseApproveFromOutside(address _seller, uint256 _buildId, uint256 _percent) public {
+        purchasesApprove[msg.sender][_seller][_buildId] = _percent;
+    }
+
+    function setPurchaseApproveFromApp(address _buyer, address _seller, uint256 _buildId, uint256 _percent) public onlyOwner {
+        purchasesApprove[_buyer][_seller][_buildId] = _percent;
     }
 
 }
--- contracts/Buildings.sol
+++ contracts/Buildings.sol
@@ -1,20 +1,30 @@
+/*
+author: Kirill Kuzin (https://github.com/kirillkuzin)
+
+TODO:
+1) Добавить координаты в структуру здания
+2) Добавить комментарии
+3) Добавить библиотеку SafeMath
+*/
+
 pragma solidity ^0.4.21;
 
 import "./Finance.sol";
 
+
 contract Buildings is Finance {
 
     struct BuildingStruct {
-        address owner;
-        bool forSale;
+        mapping(address => uint256) percentOwnership;
+        uint256 percentForSale;
         uint256 governmentPrice;
-        uint256 auctionPrice;
+        uint256 rent;
     }
     mapping(uint256 => BuildingStruct) public buildings;
     uint256 public buildingId;
 
-    modifier isBuildOwner(address _sender, uint256 _buildingId) {
-        require(_sender == buildings[_buildingId].owner);
+    modifier isBuildingOwner(uint256 _buildingId, address _sender, uint256 _percent) {
+        require(buildings[_buildingId].percentOwnership[_sender] > _percent);
         _;
     }
 
@@ -23,77 +33,89 @@ contract Buildings is Finance {
         _;
     }
 
-    modifier isBuildingForGovernmentSale(uint256 _buildingId) {
-        require(buildings[_buildingId].forSale);
+    modifier isCorrectPercentForSale(uint256 _percentForSale) {
+        require(_percentForSale <= 100);
         _;
     }
 
-    modifier isBuildingForAuctionSale(uint256 _buildingId) {
-        require(buildings[_buildingId].auctionPrice > 0);
+    modifier isBuildingForGovernmentSale(uint256 _buildingId, uint256 _percentForSale) {
+        require(buildings[_buildingId].percentForSale >= _percentForSale);
         _;
     }
 
-    function addBuild(uint256 _sellPrice) public onlyOwner returns(bool result) {
-        buildings[buildingId].owner = address(this);
-        buildings[buildingId].forSale = true;
-        buildings[buildingId].governmentPrice = _sellPrice;
+    function addBuild(uint256 _percentForSale, uint256 _sellPrice, uint256 _rent) public onlyOwner isCorrectPercentForSale(_percentForSale) returns(bool result) {
+        addBuildingPercentForSale(_buildingId, _percentForSale);
+        setBuildingGovernmentPrice(buildingId, _sellPrice);
+        setBuildingRent(buildingId, _rent);
         buildingId++;
         return true;
     }
 
-    function buyGovernmentBuildingFromOutside(uint256 _buildingId) public isBuildingForGovernmentSale(_buildingId) {
-        //TODO: transfer PWD
-        buyGovernmentBuilding(_buildingId, msg.sender);
+    function buyGovernmentBuildingFromOutside(uint256 _buildingId, uint256 _percent) public isBuildingForGovernmentSale(_buildingId, _percent) {
+        buyGovernmentBuilding(_buildingId, msg.sender, _percent);
     }
 
-    function buyGovernmentBuildingFromApp(uint256 _buildingId, address _newOwner) public onlyOwner isBuildingForGovernmentSale(_buildingId) returns(bool result) {
-        //TODO: transfer PWD
-        buyGovernmentBuilding(_buildingId, _newOwner);
+    function buyGovernmentBuildingFromApp(uint256 _buildingId, address _newOwner, uint256 _percent) public onlyOwner isBuildingForGovernmentSale(_buildingId, _percent) returns(bool result) {
+        buyGovernmentBuilding(_buildingId, _newOwner, _percent);
         return true;
     }
 
-    function sellBuilding(uint256 _buildingId, uint256 _sellPrice, address _newOwner) public isBuildOwner(msg.sender, _buildingId) isCorrectPrice(_sellPrice) isPurchaseApprove(_newOwner, msg.sender, _buildingId) {
-        //TODO: transfer PWD
-        setBuildingOwner(_buildingId, _newOwner);
+    function sellBuildingFromOutside(uint256 _buildingId, uint256 _sellPrice, address _newOwner, uint256 _percent) public isBuildingOwner(msg.sender, _buildingId, _percent) isCorrectPrice(_sellPrice) isPurchaseApprove(_newOwner, msg.sender, _buildingId, _percent) {
+        sellBuildingTx(_newOwner, msg.sender, _sellPrice);
+        addBuildingPercentOwnership(_buildingId, _newOwner, _percent);
+        subBuildingPercentOwnership(_buildingId, msg.sender, _percent);
     }
 
-    function sellBuildingFromApp(uint256 _buildingId, uint256 _sellPrice, address _newOwner) public onlyOwner isCorrectPrice(_sellPrice) returns(bool result) {
-        //TODO: transfer PWD
-        setBuildingOwner(_buildingId, _newOwner);
+    function sellBuildingFromApp(uint256 _buildingId, uint256 _sellPrice, address _newOwner, address _oldOwner, uint256 _percent) public onlyOwner isCorrectPrice(_sellPrice) returns(bool result) {
+        sellBuildingTx(_newOwner, _oldOwner, _sellPrice);
+        addBuildingPercentOwnership(_buildingId, _newOwner, _percent);
+        subBuildingPercentOwnership(_buildingId, _oldOwner, _percent);
         return true;
     }
 
-    function sellBuildingToGovernmentFromOutside(uint256 _buildingId) public isBuildOwner(msg.sender, _buildingId) {
-        //TODO: transfer PWD
-        sellBuildingToGovernment(_buildingId);
+    function sellBuildingToGovernmentFromOutside(uint256 _buildingId, uint256 _percent) public isBuildingOwner(msg.sender, _buildingId, _percent) {
+        sellBuildingToGovernment(_buildingId, msg.sender, _percent);
     }
 
-    function sellBuildingToGovernmentFromApp(uint256 _buildingId) public onlyOwner returns(bool result) {
-        //TODO: transfer PWD
-        sellBuildingToGovernment(_buildingId);
+    function sellBuildingToGovernmentFromApp(uint256 _buildingId, address _oldOwner, uint256 _percent) public onlyOwner returns(bool result) {
+        sellBuildingToGovernment(_buildingId, _oldOwner, _percent);
         return true;
     }
 
-    function buyGovernmentBuilding(uint256 _buildingId, address _newOwner) private {
-        setBuildingOwner(_buildingId, _newOwner);
-        setBuildingGovernmentSaleState(_buildingId, false);
+    function buyGovernmentBuilding(uint256 _buildingId, address _newOwner, uint256 _percent) private {
+        buyGovernmentBuildingTx(buildings[_buildingId].governmentPrice);
+        subBuildingPercentForSale(_buildingId, _percent);
+        addBuildingPercentOwnership(_buildingId, _newOwner, _percent);
     }
 
-    function sellBuildingToGovernment(uint256 _buildingId) private {
-        setBuildingOwner(_buildingId, address(this));
-        setBuildingGovernmentSaleState(_buildingId, true);
+    function sellBuildingToGovernment(uint256 _buildingId, address _oldOwner, uint256 _percent) private {
+        sellBuildingToGovernmentTx(_oldOwner, buildings[_buildingId].governmentPrice / 100 * buildings[_buildingId].percentOwnership[_oldOwner]);
+        addBuildingPercentForSale(_buildingId, _percent);
+        subBuildingPercentOwnership(_buildingId, _oldOwner, _percent);
     }
 
-    function setBuildingOwner(uint256 _buildingId, address _newOwner) private {
-        buildings[_buildingId].owner = _newOwner;
+    function setBuildingGovernmentPrice(uint256 _buildingId, uint256 _price) private {
+        buildings[_buildingId].governmentPrice = _price;
     }
 
-    function setBuildingGovernmentSaleState(uint256 _buildingId, bool _state) private {
-        buildings[_buildingId].forSale = _state;
+    function setBuildingRent(uint256 _buildingId, uint256 _rent) private {
+        buildings[_buildingId].rent = _rent;
     }
 
-    function setBuildingAuctionPrice(uint256 _buildingId, uint256 _price) private {
-        buildings[_buildingId].auctionPrice = _price;
+    function addBuildingPercentForSale(uint256 _buildingId, uint256 _percentForSale) private {
+        buildings[_buildingId].percentForSale += _percentForSale;
+    }
+
+    function subBuildingPercentForSale(uint256 _buildingId, uint256 _percentForSale) private {
+        buildings[_buildingId].percentForSale -= _percentForSale;
+    }
+
+    function addBuildingPercentOwnership(uint256 _buildingId, address _owner, uint256 _percentOwnership) private {
+        buildings[_buildingId].percentOwnership[_owner] += _percentOwnership;
+    }
+
+    function subBuildingPercentOwnership(uint256 _buildingId, address _owner, uint256 _percentOwnership) private {
+        buildings[_buildingId].percentOwnership[_owner] -= _percentOwnership;
     }
 
 }
