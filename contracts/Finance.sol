9982330f2a05e31c73a5876e5a97372db76da044
Kirill <offkirillkuzin@gmail.com>
2018-06-19 15:15:24 +0300

    Added step tx and get rent functions.

--- contracts/Finance.sol
+++ contracts/Finance.sol
@@ -6,6 +6,7 @@ TODO:
 2) Добавить операции с токеном
 3) Добавить комментарии
 4) Добавить библиотеку SafeMath
+5) Добавить кражу ренты
 */
 
 pragma solidity ^0.4.21;
@@ -14,15 +15,34 @@ import "./Approves.sol";
 
 contract Finance is Approves {
 
-    mapping(address => uint256) rentBalances;
+    mapping(address => uint256 => uint256) rentBalances; //user address => rent time => rent
     uint256 public sellBuildingTaxPercent;
+    uint256 public taxInPercent;
 
-    function Finance(uint256 _sellBuildingTaxPercent) public {
+    function Finance(uint256 _sellBuildingTaxPercent, uint256 _taxInPercent) public {
         sellBuildingTaxPercent = _sellBuildingTaxPercent;
+        taxInPercent = _taxInPercent;
     }
 
-    function stepTx() internal {
+    function stepTx(address _account, uint256 _rent, address[] _buildingOwners, uint256[] _percentOwnership) internal {
+        transferPwd(_account, address(this), _rent);
+        percentOfRent = _rent / 100;
+        tax = percentOfRent * taxInPercent;
+        ownersProfit = _rent - tax;
+        for (uint256 i = 0; i < _buildingOwners.length; i++) {
+            ownerProfit = percentOfRent * _percentOwnership[i];
+            rentBalances[_buildingOwners[i]] += ownerProfit;
+        }
+    }
 
+    function getRentFromOutside() public {
+        require(rentBalances[msg.sender] > 0);
+        transferPwd(address(this), msg.sender, rentBalances[msg.sender]);
+    }
+
+    function getRentFromApp(address _account) public onlyOwner {
+        require(rentBalances[_account] > 0);
+        transferPwd(address(this), _account, rentBalances[_account]);
     }
 
     function buyGovernmentBuildingTx(address _newOwner, uint256 _price) internal {
